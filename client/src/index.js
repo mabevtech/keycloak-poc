d1e12a3943a6c8730b8175b62eeb7b0ceacda40b
Marcel <marcel.bornancin@ambevtech.com.br>
2022-08-29 20:00:53 -0300

    Add ReactKeycloakProvider wrap around App
    with hardcoded variables for now.

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import { ReactKeycloakProvider } from '@react-keycloak/web'
+import Keycloak from 'keycloak-js'
+
+const eventLogger = (event, error) => {
+    console.log('onKeycloakEvent', event, error)
+}
+
+const tokenLogger = (tokens) => {
+    console.log('onKeycloakTokens', tokens)
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ReactKeycloakProvider
+    authClient={new Keycloak({
+        url: "http://localhost:8000/",
+        realm: "myrealm",
+        clientId: "client_id",
+        checkLoginIframe: false
+    })}
+    onEvent={eventLogger}
+    onTokens={tokenLogger}
+  >
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </ReactKeycloakProvider>
 );
