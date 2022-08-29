0e3788f38f632474aa38d277f62b7655711a2ad8
Marcel <marcel.bornancin@ambevtech.com.br>
2022-08-29 20:04:23 -0300

    Add login/logout functionality to the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom";
+import { useKeycloak } from '@react-keycloak/web'
 import './App.css';
+
+function Home() {
+  const { keycloak } = useKeycloak();
+
+  console.log("keycloak", keycloak);
+  return (
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        This is a react client.
+      </p>
+      <div className="App-link">
+        {!keycloak.authenticated && (
+          <button type="button" onClick={() => keycloak.login()}>
+            Login
+          </button>
+        )}
+        {keycloak.authenticated && (
+          <div>
+            <p>Welcome {keycloak.tokenParsed.preferred_username}!</p>
+            <button type="button" onClick={() => keycloak.logout()}>
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
