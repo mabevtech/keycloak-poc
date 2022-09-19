mabevtech/keycloak-poc

commit d1a97afda7055fae81b78726ec2539bce41c7f53

Create confidential clients with serviceAccountsEnabled
So authorization with client-backend works out-of-the-box when
the client is set to confidential.

keycloak-setup.sh
@@ -135,7 +135,7 @@ curl http://localhost:$KEYCLOAK_PORT/admin/realms/$KEYCLOAK_REALM_NAME/clients \
            "clientId":      "'"$CLIENT_ID"'",
            "secret":        "'"$CLIENT_SECRET"'",
            "publicClient":  "'"$IS_PUBLIC_CLIENT"'",
-           "serviceAccountsEnabled":       false,
+           "serviceAccountsEnabled":       "'"$IS_CONFIDENTIAL_CLIENT"'",
            "authorizationServicesEnabled": false,
            "implicitFlowEnabled":          true,
            "directAccessGrantsEnabled":    true,

commit 8bdcae8ade74ec1089c6437baa09a12fe485fe25

Fix client creation not working in keycloak-setup.sh
Bug introduced in d1a97afda. It was missing a variable declaration.

keycloak-setup.sh
@@ -127,6 +127,10 @@ echo ""
 # If the client is confidential it fails to authenticate the user,
 # as Keycloak rejects requests without a client secret.
 IS_PUBLIC_CLIENT=true
+
+# "IS_CONFIDENTIAL_CLIENT = !IS_PUBLIC_CLIENT"
+${IS_PUBLIC_CLIENT} == true && IS_CONFIDENTIAL_CLIENT=false || IS_CONFIDENTIAL_CLIENT=true
+
 curl http://localhost:$KEYCLOAK_PORT/admin/realms/$KEYCLOAK_REALM_NAME/clients \
      -H "Content-Type: application/json" \
      -H "Authorization: bearer ${TOKEN}" \