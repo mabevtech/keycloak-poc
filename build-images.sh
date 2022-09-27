ea9dfa47d1d622a94698a3c036922eaece4153ad
Marcel <marcel.bornancin@ambevtech.com.br>
2022-09-27 18:54:15 -0300

    Remove "keycloak-" prefix from client and client-backend images
    Redundant as they all start with `keycloak-poc/`.

--- build-images.sh
+++ build-images.sh
@@ -6,7 +6,7 @@
 docker build -t keycloak-poc/resource-server:1.0 --file ResourceServer.Dockerfile .
 
 # Build client-backend (back-channel) image:
-docker build -t keycloak-poc/keycloak-client-backend:1.0 --file ClientBackend.Dockerfile .
+docker build -t keycloak-poc/client-backend:1.0 --file ClientBackend.Dockerfile .
 
 # Build client image:
-docker build -t keycloak-poc/keycloak-client:1.0 --file Client.Dockerfile .
+docker build -t keycloak-poc/client:1.0 --file Client.Dockerfile .
