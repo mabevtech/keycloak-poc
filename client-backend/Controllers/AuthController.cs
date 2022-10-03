2514aefd7a4bd91b5f7557b2f6b0233e78b35cd7
Marcel <marcel.bornancin@ambevtech.com.br>
2022-10-03 16:28:45 -0300

    Create fake logout endpoint in the client-backend
    It just removes the token from the cache, but doesn't communicate with Keycloak
    to logout.

--- client-backend/Controllers/AuthController.cs
+++ client-backend/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using AmbevTech.Keycloak.Client;
+using AmbevTech.Keycloak.Client.Entities;
 
 namespace ClientApi.Controllers;
 
@@ -11,18 +14,21 @@ public class AuthController : ControllerBase
     private readonly IConfiguration _configuration;
     private readonly HttpClient _httpClient;
     private readonly ITokenManager _tokenManager;
+    private readonly IMemoryCache _cache;
 
     public AuthController(
         ILogger<AuthController> logger,
         IConfiguration configuration,
         HttpClient httpClient,
-        ITokenManager tokenManager
+        ITokenManager tokenManager,
+        IMemoryCache cache
     )
     {
         _logger = logger;
         _configuration = configuration;
         _httpClient = httpClient;
         _tokenManager = tokenManager;
+        _cache = cache;
     }
 
     // Standard flow (back-channel round trip)
@@ -48,5 +54,14 @@ public class AuthController : ControllerBase
         var result = await _tokenManager.GetToken();
         return result;
     }
+
+    [Route("logout")]
+    public Task Logout()
+    {
+        var clientId = _configuration["Keycloak:Resource"];
+        var key = $"{SsoConstants.PrefixAuth}{clientId}";
+        _cache.Remove(key);
+
+        return Task.CompletedTask;
     }
 }
