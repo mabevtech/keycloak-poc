3dad253e3ae36f457af61b0308378aceae05807d
Marcel <marcel.bornancin@ambevtech.com.br>
2022-10-03 15:51:01 -0300

    Make `reset-keycloak.sh` a bit simpler

--- reset-keycloak.sh
+++ reset-keycloak.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ## Kill and recreate the Keycloak container
-
-docker-compose stop keycloak && docker-compose rm -f keycloak && docker-compose up keycloak
+docker-compose rm -sf keycloak && docker-compose up -d keycloak
 
 # Wait for it to be available again
+echo "Waiting for keycloak to be ready..."
 while ! curl --fail --silent --head http://localhost:${KEYCLOAK_PORT}; do
     sleep 1
 done
 
 # Feed data
-source ./keycloak-data-setup.sh
+./keycloak-data-setup.sh
