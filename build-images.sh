a61072746438154cfec9a1a36b42ed42e489760f
Marcel <marcel.bornancin@ambevtech.com.br>
2022-10-04 17:43:56 -0300

    Only build docker images if they don't already exist

--- build-images.sh
+++ build-images.sh
@@ -2,11 +2,15 @@
 
 ### Build component images
 
-# Build resource-server image:
-docker build -t keycloak-poc/resource-server:1.0 --file ResourceServer.Dockerfile .
+# Build resource-server image if non existent:
+docker image inspect keycloak-poc/resource-server:1.0 -f'Resource server found' || \
+    docker build -t keycloak-poc/resource-server:1.0 --file ResourceServer.Dockerfile .
 
-# Build client-backend (back-channel) image:
-docker build -t keycloak-poc/client-backend:1.0 --file ClientBackend.Dockerfile .
+# Build client-backend (back-channel) image if non existent:
 
-# Build client image:
-docker build -t keycloak-poc/client:1.0 --file Client.Dockerfile .
+docker image inspect keycloak-poc/client-backend:1.0 -f'Client backend found' || \
+    docker build -t keycloak-poc/client-backend:1.0 --file ClientBackend.Dockerfile .
+
+# Build client image if non existent:
+docker image inspect keycloak-poc/client:1.0 -f'Client found' || \
+    docker build -t keycloak-poc/client:1.0 --file Client.Dockerfile .
