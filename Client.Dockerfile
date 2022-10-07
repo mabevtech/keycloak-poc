mabevtech/keycloak-poc

commit 155baec7b870ed3aab0999fc813a14b275c4faef

Fix Client.Dockerfile
We actually don't need a global install of react-scripts,
and we are only interested in the /src files.

Client.Dockerfile
@@ -4,9 +4,8 @@ WORKDIR /app
 COPY client/package.json .
 
 RUN npm install --silent
-RUN npm install react-scripts -g --silent
 
-COPY client/ .
+COPY client/src .
 CMD npm start
 
 EXPOSE 3000

commit 64642e6f934eb6cda713a8910a29e6ae22a4e7db

Fix client app container not running
Bug introduced in 155baec7b.

Package `react-scripts` is actually needed, and the wrong
directory was being copied to the container.

Client.Dockerfile
@@ -1,11 +1,11 @@
 FROM node:16.16.0
 WORKDIR /app
 
-COPY client/package.json .
+COPY client/package*.json .
 
 RUN npm install --silent
 
-COPY client/src .
+COPY client/ .
 CMD npm start
 
 EXPOSE 3000